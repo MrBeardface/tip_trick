bb8624a959e66e26bc536cca16282a2fcee6a42a
Mike Bukowski <michael.r.bukowski@gmail.com>
2014-07-01 15:49:25 -0400

    First commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+TipTrick::Application.config.secret_key_base = '6b2dc00589dc8b4f791b1833d2d246532434675fe9883f82f0ec964488f1070cb4a8f26de9e6b75bd8b63c599b2b7a2aa5c09078ece16d12692b556f1a7c5d3f'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+TipTrick::Application.config.session_store :cookie_store, key: '_tip_trick_session'
