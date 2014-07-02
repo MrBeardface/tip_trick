14eed9bed158cd81f0695efd6c7dd2ed1afc543b
Mike Bukowski <michael.r.bukowski@gmail.com>
2014-07-01 22:12:45 -0400

    Finish layout and routes

--- config/application.rb
+++ config/application.rb
@@ -19,5 +19,7 @@ module TipTrick
     # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     # config.i18n.default_locale = :de
+    # makes boostrap-sass compatible with the asset pipeline
+     config.assets.precompile += %w(*.png *.jpg *.jpeg *.gif)
   end
 end
